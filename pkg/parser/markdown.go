2f2059a5587b98b5cb6a13f2d269a9336f12903a
cameron <cameron@webmatter.io>
2025-11-12 21:57:27 -0600

    feat: add GFM extension support for markdown table rendering

--- pkg/parser/markdown.go
+++ pkg/parser/markdown.go
@@ -7,6 +7,7 @@ import (
 	"github.com/yuin/goldmark"
 	highlighting "github.com/yuin/goldmark-highlighting"
 	meta "github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
 	"gopkg.in/yaml.v3"
 )
@@ -22,6 +23,7 @@ func ParseMarkdown(content string) (*MarkdownDocument, error) {
 	md := goldmark.New(
 		goldmark.WithExtensions(
 			meta.Meta,
+			extension.GFM,
 			highlighting.NewHighlighting(
 				highlighting.WithStyle("monokai"),
 			),
@@ -87,6 +89,7 @@ func ParseMarkdownWithYAMLFrontmatter(content string) (*MarkdownDocument, error)
 
 	md := goldmark.New(
 		goldmark.WithExtensions(
+			extension.GFM,
 			highlighting.NewHighlighting(
 				highlighting.WithStyle("monokai"),
 			),
